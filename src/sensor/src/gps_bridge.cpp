75a465db49a140dbbad942afec1633eec8beec11
AustinJia-code <email.austin.jia@gmail.com>
2025-04-08 15:56:13 -0500

    gamepad node

---
+++ src/sensor/src/gps_bridge.cpp
@@ -0,0 +1,17 @@
+/**
+ * @file gps_bridge.cpp
+ * @brief ROS node for drone sensors
+ * @cite https://github.com/PX4/px4_ros_com/blob/main/src/examples/listeners/vehicle_gps_position_listener.cpp
+ */
+
+#include "rclcpp/rclcpp.hpp"
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  auto node = std::make_shared<rclcpp::Node>("gps_bridge");
+  RCLCPP_INFO(node->get_logger(), "Sensor node running");
+  rclcpp::spin(node);
+  rclcpp::shutdown();
+  return 0;
+}
